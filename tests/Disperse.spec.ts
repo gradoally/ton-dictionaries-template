32e3209ca7400d5b257ef6f408ed1fb3e08a7e20
Some Wallet <somewallet@gmail.com>
2024-08-11 18:54:00 +0300

    drafting

--- tests/Disperse.spec.ts
+++ tests/Disperse.spec.ts
@@ -1,8 +1,9 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
+import { Cell, toNano, fromNano } from '@ton/core';
 import { Disperse } from '../wrappers/Disperse';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
+import { time } from 'console';
 
 describe('Disperse', () => {
     let code: Cell;
@@ -13,20 +14,29 @@ describe('Disperse', () => {
 
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
-    let disperse: SandboxContract<Disperse>;
+    let disperseContract: SandboxContract<Disperse>;
 
     beforeEach(async () => {
-        blockchain = await Blockchain.create();
 
-        disperse = blockchain.openContract(Disperse.createFromConfig({}, code));
+        blockchain = await Blockchain.create();
+        blockchain.now = 100;
 
         deployer = await blockchain.treasury('deployer');
 
-        const deployResult = await disperse.sendDeploy(deployer.getSender(), toNano('0.05'));
+        disperseContract = blockchain.openContract(
+            Disperse.createFromConfig(
+                {
+                    owner_addr: deployer.address,
+                },
+                code
+            )
+        );
+
+        const deployResult = await disperseContract.sendDeploy(deployer.getSender(), toNano('0.05'));
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
-            to: disperse.address,
+            to: disperseContract.address,
             deploy: true,
             success: true,
         });
@@ -34,6 +44,44 @@ describe('Disperse', () => {
 
     it('should deploy', async () => {
         // the check is done inside beforeEach
-        // blockchain and disperse are ready to use
+        // blockchain and counter are ready to use
+    });
+
+    it('should add wallet', async () => {
+
+        let wallet_arr = await blockchain.createWallets(5);
+
+        const balance = await deployer.getBalance();
+        console.log(`Deployer Balance: ${fromNano(balance)}`);
+
+        // let i = 0;
+        for (let i = 0; i < wallet_arr.length; i++) {     
+            const walletCountBefore = await disperseContract.getWalletCount();
+            console.log(`wallet account before adding: ${walletCountBefore}`);
+
+            console.log(`adding ${wallet_arr[i].address}`);
+            const addResult = await disperseContract.sendAddWallet(deployer.getSender(), {
+                wallet_addr: wallet_arr[i].address,
+                value: toNano('1'),
+            });
+
+            expect(addResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: disperseContract.address,
+                success: true,
+            });
+
+            const walltCountAfter = await disperseContract.getWalletCount();
+            console.log(`wallet count after adding: ${walltCountAfter}`);
+
+            expect(walltCountAfter).toBe(walletCountBefore + 1);
+        }
+
+        let wallets = await disperseContract.getWallets();
+        const resultArray = [];
+        while (wallets.remaining) {
+            resultArray.push(wallets.readAddress());
+        }
+        console.log(resultArray);
     });
 });
--- wrappers/Disperse.ts
+++ wrappers/Disperse.ts
@@ -1,9 +1,20 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
-export type DisperseConfig = {};
+export const Opcodes = {
+    add_wallet: 0xf6e14824,
+    add_jetton_wallet: 0x432ff932,
+    transfer: 0x3ee943f1,
+    transfer_jetton: 0x31733dc2,
+};
+
+export type DisperseConfig = {
+    owner_addr: Address | undefined;
+};
 
 export function disperseConfigToCell(config: DisperseConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeAddress(config.owner_addr)
+    .endCell();
 }
 
 export class Disperse implements Contract {
@@ -25,5 +36,45 @@ export class Disperse implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendAddWallet(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            wallet_addr: Address;
+            value: bigint;
+            queryID?: number;
+        }
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.add_wallet, 32)
+                .storeUint(opts.queryID ?? 0, 64)
+                .storeAddress(opts.wallet_addr)
+                .endCell(),
+        });
+    }
+
+    async getOwnerAddr(provider: ContractProvider) {
+        const result = await provider.get('get_owner_addr', []);
+        return result.stack.readAddress();
+    }
+
+    async getWalletCount(provider: ContractProvider) {
+        const result = await provider.get('get_wallet_count', []);
+        return result.stack.readNumber();
+    }
+
+    async getWallets(provider: ContractProvider) {
+        const result = await provider.get('get_wallets', []);
+        return result.stack.readTuple();
+    }
+
+    async getJettonWalletCount(provider: ContractProvider) {
+        const result = await provider.get('get_jetton_wallet_count', []);
+        return result.stack.readNumber();
     }
 }
